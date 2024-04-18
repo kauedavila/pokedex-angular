7cf5c0db02341214c39f0f77144f77f5f17ef3f4
Kaue <kaueourives2@gmail.com>
2024-04-18 11:41:01 -0300

    feat: creating pokemon service

---
+++ src/app/services/pokemon/pokemon.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  apiUrl = 'https://pokeapi.co/api/v2/';
+
+  constructor(private http: HttpClient) {}
+
+  getPokemons(offset: number = 0): Observable<any> {
+    const url = `${this.apiUrl}pokemon?offset=${offset}&limit=20`;
+    return this.http.get(url);
+  }
+}
